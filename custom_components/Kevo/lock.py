26018007a856054cadccbe18782ea0395c72f76c
davidlang42 <37281992+davidlang42@users.noreply.github.com>
2020-04-13 13:34:25 +1000

    Added retry for loading error
    Often error 500 on loading:
    ``pykevoplus.KevoError: Error getting lock info: {"status":"500","error":"Internal Server Error"}``

--- custom_components/Kevo/lock.py
+++ custom_components/Kevo/lock.py
@@ -1,5 +1,5 @@
 import logging
-
+import time
 import voluptuous as vol
 
 # Import the device class from the component that you want to support
@@ -9,12 +9,16 @@ import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
 CONF_LOCK_ID = "lock_id"
+CONF_MAX_RETRIES = "max_retries"
+CONF_RETRY_DELAY = "retry_delay"
 
 # Validation of the user's configuration
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_EMAIL): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
     vol.Required(CONF_LOCK_ID): cv.string,
+    vol.Optional(CONF_MAX_RETRIES, default=3): cv.positive_int,
+    vol.Optional(CONF_RETRY_DELAY, default=2): cv.positive_int
 })
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -26,13 +30,25 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     email = config.get(CONF_EMAIL)
     password = config.get(CONF_PASSWORD)
     lock_id = config.get(CONF_LOCK_ID)
+    max_retries = config.get(CONF_MAX_RETRIES)
+    retry_delay = config.get(CONF_RETRY_DELAY)
 
     # Setup connection with devices/cloud (broken as of 9 Sep 2019 due to CAPTCHA changes)
     # kevos = Kevo.GetLocks(email, password)
     # add_devices(KevoDevice(kevo) for kevo in kevos)
 
     # Setup manual connection with specified device
-    kevo = KevoLock.FromLockID(lock_id, email, password)
+    for attempt in range(max_retries):
+        try:
+            kevo = KevoLock.FromLockID(lock_id, email, password)
+        except:
+            if attempt == max_retries - 1:
+                raise
+            else:
+                time.sleep(retry_delay)
+        else:
+            break
+    
     add_devices([KevoDevice(kevo)])
 
 class KevoDevice(LockDevice):
