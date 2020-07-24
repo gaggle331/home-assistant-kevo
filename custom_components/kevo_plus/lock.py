cac2495d6f010211686bc8f0941a4b7daf332f7e
mggingras <mggingras@cogiscan.com>
2020-07-24 11:34:00 -0400

    Cleanup, update and support for multiple locks in a single platform

--- custom_components/kevo_plus/lock.py
+++ custom_components/kevo_plus/lock.py
@@ -3,11 +3,12 @@ import time
 import voluptuous as vol
 
 # Import the device class from the component that you want to support
-from homeassistant.components.lock import LockDevice, PLATFORM_SCHEMA
-from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, STATE_LOCKED, STATE_UNLOCKED
+from homeassistant.components.lock import LockEntity, PLATFORM_SCHEMA
+from homeassistant.const import CONF_EMAIL, CONF_PASSWORD, STATE_LOCKED
 import homeassistant.helpers.config_validation as cv
 
 _LOGGER = logging.getLogger(__name__)
+CONF_LOCKS = "locks"
 CONF_LOCK_ID = "lock_id"
 CONF_MAX_RETRIES = "max_retries"
 CONF_RETRY_DELAY = "retry_delay"
@@ -16,9 +17,16 @@ CONF_RETRY_DELAY = "retry_delay"
 PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend({
     vol.Required(CONF_EMAIL): cv.string,
     vol.Required(CONF_PASSWORD): cv.string,
-    vol.Required(CONF_LOCK_ID): cv.string,
-    vol.Optional(CONF_MAX_RETRIES, default=3): cv.positive_int,
-    vol.Optional(CONF_RETRY_DELAY, default=2): cv.positive_int
+    vol.Required(CONF_LOCKS): vol.All(
+        cv.ensure_list,
+        [
+            {
+                vol.Required(CONF_LOCK_ID): cv.string,
+                vol.Optional(CONF_MAX_RETRIES, default=3): cv.positive_int,
+                vol.Optional(CONF_RETRY_DELAY, default=2): cv.positive_int
+            }
+        ]
+    )
 })
 
 def setup_platform(hass, config, add_devices, discovery_info=None):
@@ -29,29 +37,32 @@ def setup_platform(hass, config, add_devices, discovery_info=None):
     # present.
     email = config.get(CONF_EMAIL)
     password = config.get(CONF_PASSWORD)
-    lock_id = config.get(CONF_LOCK_ID)
-    max_retries = config.get(CONF_MAX_RETRIES)
-    retry_delay = config.get(CONF_RETRY_DELAY)
+    locks = config.get(CONF_LOCKS)
 
     # Setup connection with devices/cloud (broken as of 9 Sep 2019 due to CAPTCHA changes)
     # kevos = Kevo.GetLocks(email, password)
     # add_devices(KevoDevice(kevo) for kevo in kevos)
 
     # Setup manual connection with specified device
-    for attempt in range(max_retries):
-        try:
-            kevo = KevoLock.FromLockID(lock_id, email, password)
-        except:
-            if attempt == max_retries - 1:
-                raise
+    for lock in locks:
+        lock_id = lock[CONF_LOCK_ID]
+        max_retries = lock[CONF_MAX_RETRIES]
+        retry_delay = lock[CONF_RETRY_DELAY]
+        
+        for attempt in range(max_retries):
+            try:
+                kevo = KevoLock.FromLockID(lock_id, email, password)
+            except:
+                if attempt == max_retries - 1:
+                    raise
+                else:
+                    time.sleep(retry_delay)
             else:
-                time.sleep(retry_delay)
-        else:
-            break
-    
-    add_devices([KevoDevice(kevo)])
+                break
+        
+        add_devices([KevoDevice(kevo)])
 
-class KevoDevice(LockDevice):
+class KevoDevice(LockEntity):
     """Representation of a Kevo Lock."""
 
     def __init__(self, kevo):
@@ -68,7 +79,7 @@ class KevoDevice(LockDevice):
     @property
     def is_locked(self):
         """Return true if lock is locked."""
-        return self._state == "locked"
+        return self._state == STATE_LOCKED
 
     def lock(self, **kwargs):
         """Instruct the lock to lock."""
